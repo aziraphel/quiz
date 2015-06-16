ff06f9b2dc14a25220c07564032a586fbe989089
Juanan <aziraphel@gmail.com>
2015-06-16 16:52:19 +0200

    Añadido pagina de creditos

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -2,13 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var creditsController = require('../controllers/credits_controller');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+router.get('/author', creditsController.credits);
+
+/*  //Forma alternativa sin usar controllers
+router.get('/author', function (req, res, next) {
+  res.render('author', {});
+});
+*/
 
 module.exports = router;
