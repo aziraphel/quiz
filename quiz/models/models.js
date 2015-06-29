f19c2570b3d41de08355847eab691636be598067
Juanan <aziraphel@gmail.com>
2015-06-29 10:07:40 +0200

    Añadir comentarios

--- quiz/models/models.js
+++ quiz/models/models.js
@@ -30,8 +30,17 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+// Importar definicion de la tabla Comment en comment.js
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz); //cardinalidad 1
+Quiz.hasMany(Comment);  //cardinalidad N
+
 // exportar tablas
 exports.Quiz = Quiz; 
+exports.Comment = Comment; 
+
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
